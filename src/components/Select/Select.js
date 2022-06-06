843fd4f1c2ce9696d08a2750110861697fe1edb3
Patrick <patrick@meetpatrick.dev>
2022-06-06 11:25:23 -0500

    added select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -8,11 +8,51 @@ import { getDisplayedValue } from './Select.helpers';
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
-  return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+
+  return ( 
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationalBit>{displayedValue}
+      <Icon id='chevron-down' size={24} strokeWidth={2}/>
+      </PresentationalBit>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`
+
+const PresentationalBit = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  color: ${COLORS.gray700};
+  border: none;
+  padding: 12px 16px;
+  font-size: 1rem;
+  display: flex;
+  align-items: center ;
+  gap: 14px;
+
+  ${NativeSelect}:focus + & {
+    outline: 5px auto -webkit-focus-ring-color ;
+  }
+
+  ${NativeSelect}:hover + & {
+    color: black;
+  }
+`
 
 export default Select;
